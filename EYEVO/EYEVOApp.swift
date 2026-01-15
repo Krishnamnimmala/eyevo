251cbcec3cfd3db55b9d00ae5099d57a93c6c0d9
Krishnam Nimmala <meetnimmala@gmail.com>
2026-01-14 21:27:25 -0500

    Initial Commit

---
+++ EYEVO/EYEVOApp.swift
@@ -0,0 +1,17 @@
+//
+//  EYEVOApp.swift
+//  EYEVO
+//
+//  Created by Krishnam Nimmala on 1/14/26.
+//
+
+import SwiftUI
+
+@main
+struct EYEVOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
